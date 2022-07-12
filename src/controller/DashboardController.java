b255e38173f1e19962c2131f53f9e315a1f45811
Gihan42 <gihanm729@gmail.com>
2022-07-12 10:38:03 +0530

    2nd commit

--- src/controller/DashboardController.java
+++ src/controller/DashboardController.java
@@ -1,4 +1,26 @@
 package controller;
 
+import javafx.scene.control.*;
+
 public class DashboardController {
+
+    public Button btnsave;
+    public Button btnsave1;
+    public Button btnsave11;
+    public TextField txtSearchStudent;
+
+    public TableView tblStudent;
+    public TableColumn colemail;
+    public TableColumn colid;
+    public TableColumn colname;
+    public TableColumn colAddress;
+    public TableColumn colcontact;
+    public TableColumn colnic;
+
+    public TextField txtName;
+    public TextField txtEmail;
+    public TextField txtAddress;
+    public TextField txtContact;
+    public Label lblstudentid;
+    public TextField txtNic;
 }
