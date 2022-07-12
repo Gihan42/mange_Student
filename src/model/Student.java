5ec1246743624e6294ea9b0b3df027186ca00a95
Gihan42 <gihanm729@gmail.com>
2022-07-12 10:48:58 +0530

    create student model  & use lombox

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,17 @@
 package model;
 
-public class Student {
+import lombok.*;
 
+@Data
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String SId;
+    private String SName;
+    private String eMail;
+    private String contact;
+    private String Address;
 }
