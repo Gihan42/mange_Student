0ce79acf332a7e4cd0f5783371f3bb5fec2ba653
Gihan42 <gihanm729@gmail.com>
2022-07-12 10:57:43 +0530

    create TableTm

---
+++ src/tm/StudentTableTm.java
@@ -0,0 +1,17 @@
+package tm;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentTableTm {
+    private String SId;
+    private String SName;
+    private String eMail;
+    private String contact;
+    private String Address;
+}
